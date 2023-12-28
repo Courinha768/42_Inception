a8394202eaa0f429b0b1af5ec13ae1192b538a72
courinha <antoniocourinha768@gmail.com>
2023-12-28 14:38:13 +0000

    a lot

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,30 +1,9 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out $CERTS_ -subj "/C=MO/L=KH/O=1337/OU=student/CN=sahafid.42.ma"
+#!/bin/bash
 
+mkdir -p /etc/nginx/ssl/
 
-echo "
-server {
-    listen 443 ssl;
-    listen [::]:443 ssl;
-
-    #server_name www.$DOMAIN_NAME $DOMAIN_NAME;
-
-    ssl_certificate $CERTS_;
-    ssl_certificate_key /etc/ssl/private/nginx-selfsigned.key;" > /etc/nginx/sites-available/default
-
-
-echo '
-    ssl_protocols TLSv1.3;
-
-    index index.php;
-    root /var/www/html;
-
-    location ~ [^/]\.php(/|$) { 
-            try_files $uri =404;
-            fastcgi_pass wordpress:9000;
-            include fastcgi_params;
-            fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        }
-} ' >>  /etc/nginx/sites-available/default
-
+openssl genpkey -algorithm RSA -out /etc/nginx/ssl/server.key
+openssl req -new -key /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.csr -subj "/C=PT/ST=LS/L=LS/O=42/OU=student/CN=aappleto"
+openssl x509 -req -in /etc/nginx/ssl/server.csr -signkey /etc/nginx/ssl/server.key -out /etc/nginx/ssl/server.crt
 
 nginx -g "daemon off;"
