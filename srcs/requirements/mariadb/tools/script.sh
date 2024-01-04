e45884108ce211608029c2e812c983fc472e69e2
courinha <antoniocourinha768@gmail.com>
2024-01-04 15:34:37 +0000

    things

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
+mysqld --user=mysql --initialize-insecure --datadir=/var/lib/mysql
 
-
-service mysql start 
-
+mysqld --user=mysql --socket=/var/run/mysqld/mysqld.sock &
 
 echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@172.17.0.1'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@172.17.0.1'%' ;" >> db1.sql
+echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+mysqladmin shutdown
 
-mysqld
+mysqld --user=mysql --socket=/var/run/mysqld/mysqld.sock
