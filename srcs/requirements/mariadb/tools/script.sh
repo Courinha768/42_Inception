bd7d068ba21bd2a70f50d8a20d7431684377946d
courinha <antoniocourinha768@gmail.com>
2024-01-08 14:01:34 +0000

    mariadb is working

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 
-mysql
+service mysql start 
 
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+sleep 10
 
-mysql < db1.sql
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
 
-kill $(cat /run/mysqld/mysqld.pid)
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+echo "FLUSH PRIVILEGES;" | mysql
+
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
+
